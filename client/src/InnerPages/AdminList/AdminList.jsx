954342084806d10b4ffb70de99d7789d8f678d17
Gautam-2407 <gautambhatia2407@gmail.com>
2023-09-14 10:38:04 -0700

    table format done

--- client/src/InnerPages/AdminList/AdminList.jsx
+++ client/src/InnerPages/AdminList/AdminList.jsx
@@ -23,17 +23,31 @@ const AdminList = () => {
   return (
     <div>
       <h1>Admin Members</h1>
-      
-        {data && data.length > 0 ? (
-          data.map((member) => (
-            <li key={member._id}>
-              Name: {member.name}, Phone Number: {member.phone}
-            </li>
-          ))
-        ) : (
-          <li>No admin members found.</li>
-        )}
-      
+      <table>
+
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((member) => (
+            <tr key={member.id}>
+              <td>{member.name}</td>
+              <td>{member.email}</td>
+              <td>{member.phone}</td>
+              <td>
+                <a href={`/admin/edit/${member.id}`}>Edit</a>
+                <a href={`/admin/delete/${member.id}`}>Delete</a>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+       
     </div>
   );
 };
